7e20e48be0117dbd44ae2a8bb2624c9b3b9bd068
DevOpsMina <devmina635@gmail.com>
2024-07-29 17:32:40 +0545

    Mysql Connectt

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.listen(port, () => {
 });
 */
 
-const express = require('express');
+/*const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
 
@@ -115,4 +115,62 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+*/
+
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2/promise');
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'mina',
+  password: '12345',
+  database: 'mina_cli',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+app.post('/api/data', async (req, res) => {
+  const { name, description } = req.body;
+  try {
+    const connection = await pool.getConnection();
+    const [result] = await connection.query(
+      'INSERT INTO data (name, description) VALUES (?, ?)',
+      [name, description]
+    );
+    connection.release();
+    res.status(201).json({ message: 'Data added successfully' });
+  } catch (error) {
+    console.error('Error inserting data:', error);
+    res.status(500).json({ error: 'Error inserting dataa' });
+  }
+});
+
+app.get('/api/data', async (req, res) => {
+  try {
+    const connection = await pool.getConnection();
+    const [rows] = await connection.query('SELECT * FROM data');
+    connection.release();
+    res.status(200).json(rows);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Error fetching data' });
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello, Welcome to my backend server hihi!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
 
